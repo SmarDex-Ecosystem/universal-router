9963f5b5f7719480f94caea035fd565b476d4d59
Lilyan Bastien <91454320+lilyanB@users.noreply.github.com>
2024-06-17 16:20:51 +0200

    feat: validate pending actions (#7)
    * fix: change name package-lock
    
    * chore: add command and corresponding tests
    
    * chore: merge branch 'main' into feature/validate-pending-actions
    
    * feat: remove unused return
    
    * test: fix imports
    
    * test: follow comments
    
    * test: use variable

---
+++ test/integration/UsdnProtocolValidatePendingActions.t.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {
+    ProtocolAction,
+    PendingAction,
+    PreviousActionsData,
+    PositionId
+} from "usdn-contracts/src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
+
+import { PYTH_ETH_USD } from "./utils/Constants.sol";
+import { DEPLOYER, USER_1, USER_2, USER_3, USER_4 } from "./utils/Constants.sol";
+import { UniversalRouterBaseFixture } from "./utils/Fixtures.sol";
+
+import { Commands } from "../../src/libraries/Commands.sol";
+
+/**
+ * @custom:feature Validate pending actions through the router
+ * @custom:background Given a forked ethereum mainnet chain
+ */
+contract TestForkUniversalRouterValidatePendingActions is UniversalRouterBaseFixture {
+    uint256 _securityDeposit;
+    uint256 ts1;
+
+    function setUp() public {
+        _setUp();
+        // set 4 pending actions
+        ts1 = block.timestamp;
+        _securityDeposit = protocol.getSecurityDepositValue();
+        // 1. initiateDeposit
+        uint256 openPositionAmount = 2 ether;
+        deal(address(wstETH), address(this), 1e6 ether);
+        deal(address(sdex), address(this), 1e6 ether);
+        wstETH.approve(address(protocol), type(uint256).max);
+        sdex.approve(address(protocol), type(uint256).max);
+        protocol.initiateDeposit{ value: _securityDeposit }(
+            0.1 ether, USER_2, USER_1, NO_PERMIT2, "", EMPTY_PREVIOUS_DATA
+        );
+        // 2. initiateWithdrawal
+        uint256 withdrawAmount = usdn.sharesOf(DEPLOYER) / 100;
+        vm.prank(DEPLOYER);
+        usdn.transferShares(address(this), withdrawAmount);
+        usdn.approve(address(protocol), type(uint256).max);
+        protocol.initiateWithdrawal{ value: _securityDeposit }(
+            uint128(withdrawAmount), USER_2, USER_2, "", EMPTY_PREVIOUS_DATA
+        );
+        // 3. initiateOpenPosition
+        uint256 desiredLiquidation = 2500 ether;
+        protocol.initiateOpenPosition{ value: _securityDeposit }(
+            uint128(openPositionAmount),
+            uint128(desiredLiquidation),
+            address(this),
+            USER_3,
+            NO_PERMIT2,
+            "",
+            EMPTY_PREVIOUS_DATA
+        );
+        // 4. initiateClosePosition
+        (, PositionId memory posId) = protocol.initiateOpenPosition{ value: _securityDeposit }(
+            uint128(openPositionAmount),
+            uint128(desiredLiquidation),
+            address(this),
+            payable(this),
+            NO_PERMIT2,
+            "",
+            EMPTY_PREVIOUS_DATA
+        );
+        _waitDelay(); // to be realistic because not mandatory
+        (,,,, bytes memory data) = getHermesApiSignature(PYTH_ETH_USD, ts1 + oracleMiddleware.getValidationDelay());
+        uint256 validationCost = oracleMiddleware.validationCost(data, ProtocolAction.ValidateOpenPosition);
+        protocol.validateOpenPosition{ value: validationCost }(payable(this), data, EMPTY_PREVIOUS_DATA);
+        protocol.initiateClosePosition{ value: _securityDeposit }(
+            posId, uint128(openPositionAmount), USER_1, USER_4, "", EMPTY_PREVIOUS_DATA
+        );
+    }
+
+    /**
+     * @custom:scenario Validate pending actions through the router
+     * @custom:given The protocol has 4 pending actions
+     * @custom:when The user Validate pending actions through the router
+     * @custom:then Validate pending actions successfully
+     */
+    function test_ForkValidatePendingActions() public {
+        vm.makePersistent(address(protocol));
+        vm.makePersistent(address(router));
+        vm.makePersistent(address(oracleMiddleware));
+        vm.makePersistent(address(usdn));
+        vm.makePersistent(address(wstETH));
+
+        // initial fork roll to skip 20 minutes
+        vm.rollFork(block.number + 100);
+
+        // set the search range
+        uint256 startBlock = block.number;
+        uint256 endBlock = block.number + 300;
+
+        (uint80 roundId,,) = getNextChainlinkPriceAfterTimestamp(ts1, startBlock, endBlock);
+
+        skip(protocol.getValidationDeadline());
+
+        // prepare data for the validation
+        bytes memory data = abi.encode(roundId);
+        uint8 actionsCount = 4;
+        bytes[] memory priceData = new bytes[](actionsCount);
+        for (uint256 i = 0; i < actionsCount; i++) {
+            priceData[i] = data;
+        }
+        (, uint128[] memory newRawIndices) = protocol.getActionablePendingActions(address(0));
+        assertEq(newRawIndices.length, actionsCount, "newRawIndices.length");
+        PreviousActionsData memory previousActionsData =
+            PreviousActionsData({ priceData: priceData, rawIndices: newRawIndices });
+        uint256 ethBalanceBefore = address(router).balance;
+
+        // validating actionable pending actions through the router
+        bytes memory commands = abi.encodePacked(uint8(Commands.VALIDATE_PENDING));
+        bytes[] memory inputs = new bytes[](1);
+        inputs[0] = abi.encode(previousActionsData, actionsCount, 0);
+        router.execute{ value: 0 }(commands, inputs);
+
+        assertEq(address(router).balance, ethBalanceBefore + _securityDeposit * actionsCount, "ether balance");
+    }
+
+    receive() external payable { }
+}
--- test/integration/utils/Fixtures.sol
+++ test/integration/utils/Fixtures.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
+import { AggregatorV3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import { IAllowanceTransfer } from "permit2/src/interfaces/IAllowanceTransfer.sol";
 import { DEPLOYER, WETH, WSTETH } from "usdn-contracts/test/utils/Constants.sol";
 import { UsdnProtocolBaseIntegrationFixture } from "usdn-contracts/test/integration/UsdnProtocol/utils/Fixtures.sol";
@@ -15,6 +16,7 @@ import { RouterParameters } from "../../../src/base/RouterImmutables.sol";
 contract UniversalRouterBaseFixture is UsdnProtocolBaseIntegrationFixture {
     UniversalRouterHandler public router;
     IAllowanceTransfer permit2;
+    AggregatorV3Interface public priceFeed;
 
     function _setUp() internal {
         params = DEFAULT_PARAMS;
@@ -38,5 +40,46 @@ contract UniversalRouterBaseFixture is UsdnProtocolBaseIntegrationFixture {
         router = new UniversalRouterHandler(params);
 
         permit2 = IAllowanceTransfer(params.permit2);
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    }
+
+    /**
+     * @notice get the first next chainlink price after the pending action timestamp
+     * @dev binary search to find the first roundId after the pending action timestamp,
+     * revert if roundId is not the first one after the low latency limit. This function
+     * make a rollFork to the block where the roundId is found and return the roundId, price and timestamp
+     */
+    function getNextChainlinkPriceAfterTimestamp(uint256 pendingActionTimestamp, uint256 startBlock, uint256 endBlock)
+        public
+        returns (uint80 roundId_, int256 price_, uint256 timestamp_)
+    {
+        uint256 lowLatencyLimit = pendingActionTimestamp + oracleMiddleware.getLowLatencyDelay();
+        // set the search range
+        uint256 left = startBlock;
+        uint256 right = endBlock;
+
+        // perform binary search
+        while (left < right) {
+            uint256 mid = (left + right) / 2;
+            vm.rollFork(mid);
+            (roundId_,,, timestamp_,) = priceFeed.latestRoundData();
+
+            if (timestamp_ < lowLatencyLimit) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        // final fork roll to the first round after the low latency limit
+        vm.rollFork(left);
+        (roundId_, price_,, timestamp_,) = priceFeed.latestRoundData();
+
+        // ensure roundId is first one after the low latency limit
+        (,, uint256 updateAtOne,,) = priceFeed.getRoundData(roundId_ - 1);
+        (,, uint256 updateAtTwo,,) = priceFeed.getRoundData(roundId_);
+        assertTrue(updateAtOne < lowLatencyLimit, "updateAtOne < lowLatencyLimit");
+        assertTrue(updateAtTwo >= lowLatencyLimit, "updateAtTwo >= lowLatencyLimit");
+        return (roundId_, price_, timestamp_);
     }
 }
