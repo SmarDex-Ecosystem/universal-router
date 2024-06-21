2c1ae8569abfd462dd955af29aa9e81be95af16f
Fireboss <60253423+fireboss777@users.noreply.github.com>
2024-06-21 15:56:54 +0200

    feat: smardex exact in (#13)
    * feat: smardex exact in
    
    * refactor: solc version

--- src/base/RouterImmutables.sol
+++ src/base/RouterImmutables.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.25;
 
 import { IUsdnProtocol } from "usdn-contracts/src/interfaces/UsdnProtocol/IUsdnProtocol.sol";
+import { ISmardexFactory } from "src/interfaces/smardex/ISmardexFactory.sol";
 
 /**
  * @dev Structure to hold the immutable parameters for the router
@@ -13,6 +14,7 @@ import { IUsdnProtocol } from "usdn-contracts/src/interfaces/UsdnProtocol/IUsdnP
  * @param poolInitCodeHash The v3 pool hash
  * @param usdnProtocol The USDN protocol address
  * @param wstEth The WstETH address
+ * @param smardexFactory The smardex factory
  */
 struct RouterParameters {
     address permit2;
@@ -23,4 +25,5 @@ struct RouterParameters {
     bytes32 poolInitCodeHash;
     IUsdnProtocol usdnProtocol;
     address wstEth;
+    ISmardexFactory smardexFactory;
 }
