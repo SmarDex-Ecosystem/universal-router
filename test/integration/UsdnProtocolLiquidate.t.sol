d441c71c922cf51aabf8a938027170bafd41bbf7
Fireboss <60253423+fireboss777@users.noreply.github.com>
2024-06-21 16:37:40 +0200

    feat: liquidation (#10)
    * feat: liquidation
    
    * chore: slither error
    
    * chore: lib version

---
+++ test/integration/UsdnProtocolLiquidate.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import { ProtocolAction } from "usdn-contracts/src/interfaces/UsdnProtocol/IUsdnProtocolTypes.sol";
+
+import { Commands } from "../../src/libraries/Commands.sol";
+
+import { PYTH_ETH_USD } from "./utils/Constants.sol";
+import { UniversalRouterBaseFixture } from "./utils/Fixtures.sol";
+
+/**
+ * @custom:feature Test liquidate command of universal router
+ * @custom:background A initiated universal router
+ */
+contract TestForkUniversalRouterLiquidate is UniversalRouterBaseFixture {
+    function setUp() external {
+        _setUp();
+    }
+
+    /**
+     * @custom:scenario Test the `LIQUIDATE`command of the universal router
+     * @custom:given A initiated universal router
+     * @custom:and A recent pyth price
+     * @custom:when The command is executed
+     * @custom:then The transaction should be executed
+     */
+    function test_ForkExecuteLiquidate() external {
+        bytes memory commands = abi.encodePacked(uint8(Commands.LIQUIDATE));
+        (,,,, bytes memory data) = getHermesApiSignature(PYTH_ETH_USD, block.timestamp);
+        uint256 validationCost = oracleMiddleware.validationCost(data, ProtocolAction.Liquidation);
+        bytes[] memory inputs = new bytes[](1);
+        inputs[0] = abi.encode(data, 10, validationCost);
+        router.execute{ value: validationCost }(commands, inputs);
+    }
+}
