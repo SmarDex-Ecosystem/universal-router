1f70f2023eed86df0b95c2cbca79545ae0b36b80
fireboss777 <fireboss@tutanota.com>
2024-06-24 17:01:40 +0200

    feat: smardex lib

--- src/modules/smardex/SmardexImmutables.sol
+++ src/modules/smardex/SmardexImmutables.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.25;
 
 import { IWETH9 } from "@uniswap/universal-router/contracts/interfaces/external/IWETH9.sol";
+import { IAllowanceTransfer } from "permit2/src/interfaces/IAllowanceTransfer.sol";
 
 import { ISmardexFactory } from "../../interfaces/smardex/ISmardexFactory.sol";
 
@@ -9,10 +10,12 @@ import { ISmardexFactory } from "../../interfaces/smardex/ISmardexFactory.sol";
  * @notice The smardex parameters struct
  * @param smardexFactory The smardex factory
  * @param weth The wrapped ETH address
+ * @param permit2 The permit2 address
  */
 struct SmardexParameters {
     ISmardexFactory smardexFactory;
     address weth;
+    address permit2;
 }
 
 contract SmardexImmutables {
@@ -22,9 +25,13 @@ contract SmardexImmutables {
     /// @dev The wrapped ETH
     IWETH9 internal immutable WETH;
 
+    /// @dev The permit2 contract
+    IAllowanceTransfer internal immutable SMARDEX_PERMIT2;
+
     /// @param params The smardex parameters
     constructor(SmardexParameters memory params) {
         SMARDEX_FACTORY = params.smardexFactory;
         WETH = IWETH9(params.weth);
+        SMARDEX_PERMIT2 = IAllowanceTransfer(params.permit2);
     }
 }
