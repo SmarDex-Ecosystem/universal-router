11ffa4ea32c948d87b9724a9a9711cb20e642bdb
Alfred Gaillard <alfred.gaillard.pro@gmail.com>
2024-06-26 11:14:41 +0200

    docs: update test/integration/Permit.t.sol
    Co-authored-by: Fireboss <60253423+fireboss777@users.noreply.github.com>

--- test/integration/Permit.t.sol
+++ test/integration/Permit.t.sol
@@ -98,7 +98,7 @@ contract TestForkUniversalRouterPermit is UniversalRouterBaseFixture, SigUtils {
     }
 
     /**
-     * @custom:scenario An attackant steals the signature of a permit approval to transfer assets
+     * @custom:scenario An attacker steals the signature of a permit approval to transfer assets
      * @custom:given The user has 1 `wstETH`
      * @custom:when The user initiates a permit through the router without any flag
      * @custom:and An attacker steals the signature of the user and front-runs the permit approval through the router
