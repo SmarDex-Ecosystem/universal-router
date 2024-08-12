366946ff7ec3827ac317b4c8be1d1c0e4291248d
Valentin B. <703631+beeb@users.noreply.github.com>
2024-08-12 10:03:29 +0200

    chore: update dependencies and move to soldeer (#36)
    * chore: update deps
    
    * chore: fix husky script
    
    * chore: migrate to soldeer for deps
    
    * chore: remove remappings for natspec
    
    * ci: fix install commands
    
    * ci: try to clone usdn-contracts with soldeer
    
    * ci: try to clone usdn-contracts from soldeer
    
    * ci: try to clone usdn-contracts from soldeer
    
    * ci: try to rewrite url in foundry.toml
    
    * ci: try again
    
    * ci: try global
    
    * ci: try again
    
    * ci: last try
    
    * ci: i'm not going to give up
    
    * ci: fix bash variable
    
    * ci: add username
    
    * ci: test something else
    
    * ci: again
    
    * ci: might work, who knows
    
    * ci: again
    
    * ci: try different username
    
    * ci: probably not it
    
    * ci: found the problem i think
    
    * ci: YES!
    
    * ci: cleanup

--- script/deployAllFork.sh
+++ script/deployAllFork.sh
@@ -6,13 +6,13 @@ pushd $SCRIPT_DIR/..
 pushd lib/usdn-contracts/
 
 npm ci
-forge install
+forge soldeer install
 
 script/deployFork.sh
 
 popd
 npm ci
-forge install
+forge soldeer install
 
 export USDN=$(cat lib/usdn-contracts/broadcast/02_Deploy.s.sol/"$FORK_CHAIN_ID"/run-latest.json | jq -r '.returns.Usdn_.value')
 export USDN_PROTOCOL=$(cat lib/usdn-contracts/broadcast/02_Deploy.s.sol/"$FORK_CHAIN_ID"/run-latest.json | jq -r '.returns.UsdnProtocol_.value')
