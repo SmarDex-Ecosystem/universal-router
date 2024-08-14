f672b119f1f71c954bbf7f22458bff7605c2032b
beeb <703631+beeb@users.noreply.github.com>
2024-08-14 16:46:00 +0200

    chore: via-ir in fork deployment script

--- script/deployAllFork.sh
+++ script/deployAllFork.sh
@@ -23,8 +23,8 @@ deployerPrivateKey=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f
 chainId=$(cast chain-id -r "$rpcUrl")
 
 export WUSDN_ADDRESS=$(cat dependencies/usdn-contracts/broadcast/01_Deploy.s.sol/"$chainId"/run-latest.json | jq -r '.returns.Wusdn_.value')
-export USDN_PROTOCOL_ADDRESS=$(cat lib/usdn-contracts/broadcast/02_Deploy.s.sol/"$chainId"/run-latest.json | jq -r '.returns.UsdnProtocol_.value')
+export USDN_PROTOCOL_ADDRESS=$(cat lib/usdn-contracts/broadcast/01_Deploy.s.sol/"$chainId"/run-latest.json | jq -r '.returns.UsdnProtocol_.value')
 
-forge script --non-interactive --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy --broadcast
+forge script --via-ir --non-interactive --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy --broadcast
 
 popd  > /dev/null
