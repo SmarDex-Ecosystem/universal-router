05c06ef58644eb6a9ba2cb3b466bb3d8b547a5ce
beeb <703631+beeb@users.noreply.github.com>
2024-08-14 16:32:39 +0200

    chore: deployment script overhaul

---
+++ script/deploy.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+# Path of the script folder (so that the script can be invoked from somewhere else than the project's root)
+SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+
+pushd $SCRIPT_DIR/.. > /dev/null
+
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+nc='\033[0m'
+
+read -p $'\n'"Enter rpc url : " userRpcUrl
+rpcUrl=$userRpcUrl
+deployerPrivateKey=""
+address=""
+
+while true; do
+    read -p $'\n'"Do you wish to use a ledger? (Yy/Nn) : " yn
+    case $yn in
+    [Yy]*)
+        read -p $'\n'"Enter address : " userAddress
+        address=$userAddress
+
+        printf "\n\n$green Running script in Ledger mode\n"
+        ledger=true
+        break
+        ;;
+    [Nn]*)
+        read -s -p $'\n'"Enter private key : " privateKey
+        deployerPrivateKey=$privateKey
+
+        address=$(cast wallet address $deployerPrivateKey)
+        if [[ -z $address ]]; then
+            printf "\n$red The private key is invalid$nc\n\n"
+            exit 1
+        fi
+
+        printf "\n\n$green Running script in Non-Ledger mode\n"
+        ledger=false
+        break
+        ;;
+    *) printf "\nPlease answer yes (Y/y) or no (N/n).\n" ;;
+    esac
+done
+
+printf "\n$blue Address :$nc $address"
+printf "\n$blue RPC URL :$nc "$rpcUrl"\n"
+export DEPLOYER_ADDRESS=$address
+
+while true; do
+    read -p $'\n'"Do you wish to continue? (Yy/Nn) : " yn
+    case $yn in
+    [Yy]*)
+        break
+        ;;
+    [Nn]*)
+        exit 1
+        ;;
+    *) printf "\nPlease answer yes (Y/y) or no (N/n).\n" ;;
+    esac
+done
+
+if [ $ledger = true ]; then
+    forge script -l -f "$rpcUrl" script/01_Deploy.s.sol:Deploy --broadcast
+else
+    forge script --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy --broadcast
+fi
+
+popd  > /dev/null
--- script/deployAllFork.sh
+++ script/deployAllFork.sh
@@ -2,21 +2,29 @@
 # Path of the script folder (so that the script can be invoked from somewhere else than the project's root)
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
-pushd $SCRIPT_DIR/..
-pushd lib/usdn-contracts/
+pushd $SCRIPT_DIR/.. > /dev/null
+
+# Deploy USDN contracts
+pushd lib/usdn-contracts/ > /dev/null
 
 npm ci
 forge soldeer install
 
 script/deployFork.sh
 
-popd
+popd  > /dev/null
+
+# Deploy Router
 npm ci
 forge soldeer install
 
-export USDN=$(cat lib/usdn-contracts/broadcast/02_Deploy.s.sol/"$FORK_CHAIN_ID"/run-latest.json | jq -r '.returns.Usdn_.value')
-export USDN_PROTOCOL=$(cat lib/usdn-contracts/broadcast/02_Deploy.s.sol/"$FORK_CHAIN_ID"/run-latest.json | jq -r '.returns.UsdnProtocol_.value')
+rpcUrl=http://localhost:8545
+deployerPrivateKey=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+chainId=$(cast chain-id -r "$rpcUrl")
 
-script/deployFork.sh
+export WUSDN_ADDRESS=$(cat dependencies/usdn-contracts/broadcast/01_Deploy.s.sol/"$chainId"/run-latest.json | jq -r '.returns.Wusdn_.value')
+export USDN_PROTOCOL_ADDRESS=$(cat lib/usdn-contracts/broadcast/02_Deploy.s.sol/"$chainId"/run-latest.json | jq -r '.returns.UsdnProtocol_.value')
 
-popd
+forge script --non-interactive --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy --broadcast
+
+popd  > /dev/null
