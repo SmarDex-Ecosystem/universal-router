10da5f375cc4a8b3f4b34b4ba067b5ec32582d72
beeb <703631+beeb@users.noreply.github.com>
2024-08-15 14:24:50 +0200

    chore: fix deployFork

--- script/deployFork.sh
+++ script/deployFork.sh
@@ -1,29 +1,59 @@
 #!/usr/bin/env bash
+red='\033[0;31m'
+green='\033[0;32m'
+blue='\033[0;34m'
+nc='\033[0m'
+
+
 # Path of the script folder (so that the script can be invoked from somewhere else than the project's root)
 SCRIPT_DIR=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
 
+# Enter usdn-contracts folder
 pushd $SCRIPT_DIR/.. > /dev/null
 
 # Deploy USDN contracts
-pushd dependencies/usdn-contracts/ > /dev/null
+pushd dependencies/@smardex-usdn-contracts-0.17.1 > /dev/null
 
 npm ci
 forge soldeer install
 
 script/deployFork.sh
 
-# Deploy Router
-popd  > /dev/null
-
-npm ci
-forge soldeer install
-
 rpcUrl=http://localhost:8545
 deployerPrivateKey=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
 chainId=$(cast chain-id -r "$rpcUrl")
+broadcast="broadcast/01_Deploy.s.sol/$chainId/run-latest.json"
 
-export WUSDN_ADDRESS=$(cat dependencies/usdn-contracts/broadcast/01_Deploy.s.sol/"$chainId"/run-latest.json | jq -r '.returns.Wusdn_.value')
-export USDN_PROTOCOL_ADDRESS=$(cat lib/usdn-contracts/broadcast/01_Deploy.s.sol/"$chainId"/run-latest.json | jq -r '.returns.UsdnProtocol_.value')
+printf "$green USDN protocol has been deployed !\n"
+printf " Waiting for confirmation... (12s) $nc\n"
+sleep 12s
+
+for i in {1..15}; do
+    printf "$green Trying to fetch WUSDN address... (attempt $i/15)$nc\n"
+    WUSDN_ADDRESS=$(cat "$broadcast" | jq -r '.returns.Wusdn_.value')
+    wusdnCode=$(cast code -r "$rpcUrl" "$WUSDN_ADDRESS")
+
+    if [[ ! -z $wusdnCode ]]; then
+        printf "\n$green WUSDN contract found on blockchain$nc\n\n"
+        export WUSDN_ADDRESS=$WUSDN_ADDRESS
+        export USDN_PROTOCOL_ADDRESS=$(cat "$broadcast" | jq -r '.returns.UsdnProtocol_.value')
+        break
+    fi
+
+    if [ $i -eq 15 ]; then
+        printf "\n$red Failed to fetch WUSDN address$nc\n\n"
+        exit 1
+    fi
+
+    sleep 2s
+done
+
+# Enter universal-router folder
+popd  > /dev/null
+
+# Deploy Router
+npm ci
+forge soldeer install
 
 forge script --via-ir --non-interactive --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy --broadcast
 
