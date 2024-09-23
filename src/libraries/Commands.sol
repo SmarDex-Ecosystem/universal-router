dd74d964a1444aa3d87354a79d9c59c1534d0a1e
fireboss777 <fireboss@tutanota.com>
2024-09-23 16:14:50 +0200

    feat: transferShares

--- src/libraries/Commands.sol
+++ src/libraries/Commands.sol
@@ -50,10 +50,11 @@ library Commands {
     // the following constant defines one of the boundaries where the if blocks split commands
     uint256 constant THIRD_IF_BOUNDARY = 0x1a;
 
-    uint256 constant WRAP_USDN = 0x1b;
-    uint256 constant UNWRAP_WUSDN = 0x1c;
-    uint256 constant WRAP_STETH = 0x1d;
-    uint256 constant UNWRAP_WSTETH = 0x1e;
+    uint256 constant WRAP_USDN = 0x1a;
+    uint256 constant UNWRAP_WUSDN = 0x1b;
+    uint256 constant WRAP_STETH = 0x1c;
+    uint256 constant UNWRAP_WSTETH = 0x1d;
+    uint256 constant USDN_TRANSFER_SHARES_FROM = 0x1e;
     // COMMAND_PLACEHOLDER = 0x1f;
 
     // the commands are executed in nested if blocks to minimize gas consumption
