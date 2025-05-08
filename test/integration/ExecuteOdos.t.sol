999436158adaf322aa31a6645e59d9eb4afe2621
samooyo <samidarnaud@gmail.com>
2025-05-08 10:51:16 +0200

    test: refactor odos test

--- test/integration/ExecuteOdos.t.sol
+++ test/integration/ExecuteOdos.t.sol
@@ -5,6 +5,7 @@ import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 import { UniversalRouterBaseFixture } from "./utils/Fixtures.sol";
 import { WETH } from "./utils/Constants.sol";
+import { IOdosRouterV2 } from "./utils/IOdosRouterV2.sol";
 
 import { Commands } from "../../src/libraries/Commands.sol";
 import { Odos } from "../../src/modules/Odos.sol";
@@ -14,12 +15,18 @@ import { Odos } from "../../src/modules/Odos.sol";
  * @custom:background A initiated universal router
  */
 contract TestForkOdos is UniversalRouterBaseFixture {
+    uint256 internal constant SWAP_AMOUNT = 1 ether;
+    uint32 internal constant SWAP_REFERRAL_CODE = 0;
+    address internal constant SWAP_EXECUTOR = 0x76edF8C155A1e0D9B2aD11B04d9671CBC25fEE99;
+    bytes internal constant SWAP_PATH =
+        hex"010206006801010102030405ff00000000000000000000000000000000000000c4ce391d82d164c166df9c8336ddf84206b2f8127f39c581f595b53c5cb19bd0b3f8da6c935e2ca0c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2775f661b0bd1739349b9a2a3ef60be277c5d2d290fe906e030a44ef24ca8c7dc7b7c53a6c4f00ce9";
+
     function setUp() external {
         SetUpParams memory params = DEFAULT_PARAMS;
         params.forkBlock = 22_417_713;
         _setUp(params);
 
-        deal(address(wstETH), address(this), 10 ether);
+        deal(address(wstETH), address(this), SWAP_AMOUNT);
     }
 
     /**
@@ -29,20 +36,31 @@ contract TestForkOdos is UniversalRouterBaseFixture {
      * @custom:then The `ODOS` command should swap the tokens
      */
     function test_forkOdosSwap() external {
-        wstETH.transfer(address(router), 1 ether);
-        uint256 balanceWethBefore = IERC20(WETH).balanceOf(address(this));
+        wstETH.transfer(address(router), SWAP_AMOUNT);
+        uint256 balanceWethBefore = IERC20(WETH).balanceOf(address(router));
 
-        // The data is created via Odos API to swap 1 wstETH to WETH
-        bytes memory data =
-            hex"83bd37f900017f39c581f595b53c5cb19bd0b3f8da6c935e2ca00001c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2080de0b6b3a76400000810ab38f02fa1770000c49b000176edF8C155A1e0D9B2aD11B04d9671CBC25fEE99000000017FA9385bE102ac3EAc297483Dd6233D62b3e1496000000000402020300070101010293d199263632a4ef4bb438f1feb99e57b4b5f0bd0000000000000000000005c2ff000000000000000000000000000000000000000000007f39c581f595b53c5cb19bd0b3f8da6c935e2ca0c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000000000000000000000000000";
+        IOdosRouterV2.swapTokenInfo memory params = IOdosRouterV2.swapTokenInfo({
+            inputToken: address(wstETH),
+            inputAmount: SWAP_AMOUNT,
+            inputReceiver: SWAP_EXECUTOR,
+            outputToken: WETH,
+            outputQuote: SWAP_AMOUNT,
+            outputMin: SWAP_AMOUNT,
+            outputReceiver: address(router)
+        });
+
         bytes memory commands = abi.encodePacked(uint8(Commands.ODOS));
         bytes[] memory inputs = new bytes[](1);
-        inputs[0] = abi.encode(0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0, 1 ether, data);
+        bytes memory swapData =
+            abi.encodeWithSelector(IOdosRouterV2.swap.selector, params, SWAP_PATH, SWAP_EXECUTOR, SWAP_REFERRAL_CODE);
+        inputs[0] = abi.encode(address(wstETH), SWAP_AMOUNT, swapData);
 
         router.execute(commands, inputs);
 
         assertEq(wstETH.balanceOf(address(router)), 0, "wstETH balance should be zero");
-        assertGt(IERC20(WETH).balanceOf(address(this)), balanceWethBefore, "WETH balance should be greater than before");
+        assertGt(
+            IERC20(WETH).balanceOf(address(router)), balanceWethBefore, "WETH balance should be greater than before"
+        );
     }
 
     /**
