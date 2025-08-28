d08203b24446460086b7d41a5c0f281781ece5ae
Alfred GAILLARD <a.gaillard@ra2.tech>
2025-08-28 16:18:14 +0200

    feat: getting ready to deploy shortdn router in fork env

---
+++ script/deployShortdnFork.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+red='\033[0;31m'
+green='\033[0;32m'
+nc='\033[0m'
+
+script/fork/deployFork.sh
+
+rpcUrl=http://localhost:8545
+deployerPrivateKey=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+chainId=$(cast chain-id -r "$rpcUrl")
+usdnProtocolBroadcast="./broadcast/DeployUsdnWstethFork.s.sol/$chainId/run-latest.json"
+export DEPLOYER_ADDRESS=$(cast wallet address "$deployerPrivateKey")
+
+printf "$green USDN protocol has been deployed !\n"
+sleep 1s
+
+# Add USDN protocol address to .env.fork of universal-router
+cat ".env.fork" >"../../.env.fork"
+
+# Enter universal-router folder
+popd >/dev/null || exit
+
+# Deploy Router
+forge script --non-interactive --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy \
+    --broadcast --sig "run(address,address)" "0x0000000000000000000000000000000000000000" "$USDN_PROTOCOL_SHORTDN_ADDRESS"
+
+# Check logs
+DEPLOYMENT_LOG=$(cat "broadcast/01_Deploy.s.sol/$chainId/run-latest.json")
+FORK_ENV_DUMP=$(
+    cat <<EOF
+$(cat .env.fork)
+UNIVERSAL_ROUTER_SHORTDN_ADDRESS=$(echo "$DEPLOYMENT_LOG" | jq '.returns.universalRouter_.value' | xargs printf "%s\n")
+EOF
+)
+
+echo "$FORK_ENV_DUMP" >.env.fork
--- script/deployUsdnFork.sh
+++ script/deployUsdnFork.sh
@@ -17,25 +17,6 @@ export DEPLOYER_ADDRESS=$(cast wallet address "$deployerPrivateKey")
 printf "$green USDN protocol has been deployed !\n"
 sleep 1s
 
-for i in {1..15}; do
-    printf "$green Trying to fetch WUSDN address... (attempt $i/15)$nc\n"
-    WUSDN_ADDRESS=$(cat "$usdnProtocolBroadcast" | jq -r '.returns.wusdn_.value')
-    wusdnCode=$(cast code -r "$rpcUrl" "$WUSDN_ADDRESS")
-
-    if [[ ! -z $wusdnCode ]]; then
-        printf "\n$green WUSDN contract found on blockchain$nc\n\n"
-        USDN_PROTOCOL_ADDRESS=$(cat "$usdnProtocolBroadcast" | jq -r '.returns.usdnProtocol_.value')
-        break
-    fi
-
-    if [ $i -eq 15 ]; then
-        printf "\n$red Failed to fetch WUSDN address$nc\n\n"
-        exit 1
-    fi
-
-    sleep 2s
-done
-
 # Add USDN protocol address to .env.fork of universal-router
 cat ".env.fork" >"../../.env.fork"
 
@@ -44,14 +25,14 @@ popd >/dev/null || exit
 
 # Deploy Router
 forge script --non-interactive --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy \
-    --broadcast --sig "run(address,address)" "$WUSDN_ADDRESS" "$USDN_PROTOCOL_ADDRESS"
+    --broadcast --sig "run(address,address)" "$WUSDN_TOKEN_ADDRESS" "$USDN_PROTOCOL_USDN_ADDRESS"
 
 # Check logs
 DEPLOYMENT_LOG=$(cat "broadcast/01_Deploy.s.sol/$chainId/run-latest.json")
 FORK_ENV_DUMP=$(
     cat <<EOF
 $(cat .env.fork)
-UNIVERSAL_ROUTER_ADDRESS=$(echo "$DEPLOYMENT_LOG" | jq '.returns.universalRouter_.value' | xargs printf "%s\n")
+UNIVERSAL_ROUTER_USDN_ADDRESS=$(echo "$DEPLOYMENT_LOG" | jq '.returns.universalRouter_.value' | xargs printf "%s\n")
 EOF
 )
 
