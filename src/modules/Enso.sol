3c9d40a281d53594908cd58037eae8dab013b4af
samooyo <samidarnaud@gmail.com>
2025-11-13 15:52:49 +0100

    test(enso): eth swaps

--- src/modules/Enso.sol
+++ src/modules/Enso.sol
@@ -24,21 +24,21 @@ abstract contract Enso {
      * @param data The data to send to the Enso router.
      */
     function swapViaEnso(address tokenIn, uint256 ethAmount, bytes memory data) internal {
-        // if (ethAmount == 0) {
-        // forceApprove is not needed because the allowance is reset to 0 after each swap
-        IERC20(tokenIn).approve(ENSO_V2_ROUTER, type(uint256).max);
+        if (ethAmount == 0) {
+            // forceApprove is not needed because the allowance is reset to 0 after each swap
+            IERC20(tokenIn).approve(ENSO_V2_ROUTER, type(uint256).max);
 
-        (bool success,) = ENSO_V2_ROUTER.call{ value: msg.value }(data);
-        if (!success) {
-            revert EnsoSwapFailed();
+            (bool success,) = ENSO_V2_ROUTER.call{ value: msg.value }(data);
+            if (!success) {
+                revert EnsoSwapFailed();
+            }
+
+            IERC20(tokenIn).approve(ENSO_V2_ROUTER, 0);
+        } else {
+            (bool success,) = ENSO_V2_ROUTER.call{ value: ethAmount }(data);
+            if (!success) {
+                revert EnsoSwapFailed();
+            }
         }
-
-        // IERC20(tokenIn).approve(ENSO_V2_ROUTER, 0);
-        // } else {
-        //     (bool success,) = ENSO_V2_ROUTER.call{ value: ethAmount }(data);
-        //     if (!success) {
-        //         revert OdosSwapFailed();
-        //     }
-        // }
     }
 }
