2eca079499efa91f51f3f09d747e47450f28dc68
Alfred Gaillard <alfred.gaillard.pro@gmail.com>
2026-01-13 16:17:51 +0100

    feat(fork): add verify flag (#117)
    * feat(fork): add verify flag
    
    * chore: remove useless .env.fork
    
    * docs: add a comment
    
    * docs: improve comments

--- script/deployRouterShortdnFork.sh
+++ script/deployRouterShortdnFork.sh
@@ -3,14 +3,26 @@ red='\033[0;31m'
 green='\033[0m'
 nc='\033[0m'
 
+# Declare args
+VERIFY_FLAG=""
+USDN_PROTOCOL_SHORTDN_ADDRESS=""
+
+# Parse arguments, --verify is optional and can be in any position
+# the other arg is required (USDN_PROTOCOL_SHORTDN_ADDRESS).
+for arg in "$@"; do
+    if [ "$arg" == "--verify" ]; then
+        VERIFY_FLAG="--verify"
+    elif [ -z "$USDN_PROTOCOL_SHORTDN_ADDRESS" ]; then
+        USDN_PROTOCOL_SHORTDN_ADDRESS="$arg"
+    fi
+done
+
 # Check if USDN_PROTOCOL_SHORTDN_ADDRESS argument is provided
-if [ -z "$1" ]; then
+if [ -z "$USDN_PROTOCOL_SHORTDN_ADDRESS" ]; then
     printf "${red}Error: USDN_PROTOCOL_SHORTDN_ADDRESS argument is required${nc}\n"
-    printf "Usage: $0 <USDN_PROTOCOL_SHORTDN_ADDRESS>\n"
+    printf "Usage: $0 <USDN_PROTOCOL_SHORTDN_ADDRESS> [--verify]\n"
     exit 1
 fi
-
-USDN_PROTOCOL_SHORTDN_ADDRESS=$1
 
 rpcUrl=http://localhost:8545
 deployerPrivateKey=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
@@ -19,7 +31,7 @@ chainId=$(cast chain-id -r "$rpcUrl")
 
 # Deploy Router
 forge script --non-interactive --private-key "$deployerPrivateKey" -f "$rpcUrl" script/01_Deploy.s.sol:Deploy \
-    --broadcast --sig "run(address,address)" "$ADDRESS_ZERO" "$USDN_PROTOCOL_SHORTDN_ADDRESS"
+    --broadcast $VERIFY_FLAG --sig "run(address,address)" "$ADDRESS_ZERO" "$USDN_PROTOCOL_SHORTDN_ADDRESS"
 
 printf "$green SHORTDN Router has been deployed !\n"
 
